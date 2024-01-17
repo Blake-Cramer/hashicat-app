86cfc3e4d0ed064fbfbc714478251b526a83c6a0
Blake-Cramer <Blake.Cramer@conocophillips.com>
2024-01-17 21:55:57 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Cat heaven is a beautiful place, but you don’t get there if you’re euthanized.
   <!-- END -->
   
   </div>
